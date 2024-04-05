698a952317d83b70264c42c0d61721cc60af577e
Key <114710157+keykonquest@users.noreply.github.com>
2024-04-05 16:34:15 -0400

    Correct indentation

--- main.js
+++ main.js
@@ -11,18 +11,19 @@ function init() {
 function showImage() {
     let numbers = ['zero', 'one', 'two', 'three', 'four', 'five'];
     if (current <= numbers.length) {
-    let heading = document.getElementById('headline');
-    heading.innerHTML = "Did you say:";
-    let subText = document.getElementById('subline');
-    subText.innerHTML = "";
-    let number = numbers[current]
-    let image = document.getElementById('image');
-    image.src = `Images/${number}.jpg`
-    setTimeout(() => {
-        nextImage()}, 3500);
-    } else {
-        console.log('Game Over')
-        return false
+       let heading = document.getElementById('headline');
+       heading.innerHTML = "Did you say:";
+       let subText = document.getElementById('subline');
+       subText.innerHTML = "";
+       let number = numbers[current]
+       let image = document.getElementById('image');
+       image.src = `Images/${number}.jpg`
+       setTimeout(() => {
+           nextImage()}, 3500);
+    } 
+    else {
+       console.log('Game Over')
+       return false
     }
 }
 
